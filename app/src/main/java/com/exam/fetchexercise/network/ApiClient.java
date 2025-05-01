b6de22c1b662f1d321c1c0acf21f03bc795ffcbc
saiprudvi0102 <116480334+saiprudvi0102@users.noreply.github.com>
2025-05-01 14:09:18 -0400

    Update ApiClient.java

--- app/src/main/java/com/exam/fetchexercise/network/ApiClient.java
+++ app/src/main/java/com/exam/fetchexercise/network/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://fetch-hiring.s3.amazonaws.com/";
+    private static final String BASE_URL = "https://hiring.fetch.com/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
